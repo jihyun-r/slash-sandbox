cde08103083a3a61b8082ae37323a018ce5352b5
jpantaleoni@gmail.com <jpantaleoni@gmail.com@0b0c87b0-e858-3a96-bd32-8cfd005e83f8>
2011-10-23 10:02:39 +0000

    added binary tree and octree structures and (CUDA) generators

---
+++ trunk/ slash-sandbox/nih/bintree/bintree_node.h
@@ -0,0 +1,74 @@
+/*
+ * Copyright (c) 2010-2011, NVIDIA Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of NVIDIA Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#pragma once
+
+#include <nih/basic/types.h>
+
+namespace nih {
+
+///
+/// A middle-split binary tree node.
+/// A node can either be a leaf and have no children, or be
+/// an internal split node. If a split node, it can either
+/// have one or two children: for example, it can have one
+/// if a set of points is concentrated in one half-space.
+///
+struct Bintree_node
+{
+    /// empty constructor
+    NIH_HOST_DEVICE Bintree_node() {}
+
+    /// full constructor
+    NIH_HOST_DEVICE Bintree_node(bool child0, bool child1, uint32 index) :
+        m_packed_info( (child0 ? 1u : 0u) | (child1 ? 2u : 0u) | (index << 2) ) {}
+
+    /// is a leaf?
+    NIH_HOST_DEVICE uint32 is_leaf() const
+    {
+        return (m_packed_info & 3u) == 0u;
+    }
+    /// get offset of the first child
+    NIH_HOST_DEVICE uint32 get_child_offset() const
+    {
+        return m_packed_info >> 2u;
+    }
+    /// get i-th child
+    NIH_HOST_DEVICE uint32 get_child(const uint32 i) const
+    {
+        return get_child_offset() + i;
+    }
+    /// is the i-th child active?
+    NIH_HOST_DEVICE bool is_active(const uint32 i) const
+    {
+        return m_packed_info & (1u << i) ? true : false;
+    }
+
+    uint32 m_packed_info;
+};
+
+} // namespace nih
---
+++ trunk/ slash-sandbox/nih/octree/octree_test.cu
@@ -0,0 +1,88 @@
+/*
+ * Copyright (c) 2010-2011, NVIDIA Corporation
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of NVIDIA Corporation nor the
+ *     names of its contributors may be used to endorse or promote products
+ *     derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <nih/octree/cuda/octree_builder.h>
+#include <nih/sampling/random.h>
+#include <nih/time/timer.h>
+
+namespace nih {
+
+void octree_test()
+{
+    fprintf(stderr, "octree test... started\n");
+
+    const uint32 n_points = 4*1024*1024;
+    const uint32 n_tests = 100;
+
+    thrust::host_vector<Vector4f> h_points( n_points );
+
+    Random random;
+    for (uint32 i = 0; i < n_points; ++i)
+        h_points[i] = Vector4f( random.next(), random.next(), random.next(), 1.0f );
+    thrust::device_vector<Vector4f> d_points( h_points );
+    thrust::device_vector<Vector4f> d_unsorted_points( h_points );
+
+    Octree_builder builder;
+
+    cudaEvent_t start, stop;
+    cudaEventCreate( &start );
+    cudaEventCreate( &stop );
+
+    float time = 0.0f;
+
+    for (uint32 i = 0; i <= n_tests; ++i)
+    {
+        d_points = d_unsorted_points;
+        cudaThreadSynchronize();
+
+        float dtime;
+        cudaEventRecord( start, 0 );
+
+        builder.build(
+            Bbox3f( Vector3f(0.0f), Vector3f(1.0f) ),
+            d_points,
+            16u );
+
+        cudaEventRecord( stop, 0 );
+        cudaEventSynchronize( stop );
+        cudaEventElapsedTime( &dtime, start, stop );
+
+        if (i) // skip the first run
+            time += dtime;
+    }
+    time /= 1000.0f * float(n_tests);
+
+    cudaEventDestroy( start );
+    cudaEventDestroy( stop );
+
+    fprintf(stderr, "octree test... done\n");
+    fprintf(stderr, "  time       : %f ms\n", time * 1000.0f );
+    fprintf(stderr, "  points/sec : %f M\n", (n_points / time) / 1.0e6f );
+}
+
+} // namespace nih
+
