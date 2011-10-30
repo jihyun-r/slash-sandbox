d1e9a0d1caf8f0b24fa58dabc3a4d02d52502943
jpantaleoni <jpantaleoni@gmail.com>
2011-10-30 10:42:54 +0000

    small changes

---  slash-sandbox/nih/spherical/sh.h
+++  slash-sandbox/nih/spherical/sh.h
@@ -34,7 +34,24 @@
 
 namespace nih {
 
-NIH_HOST NIH_DEVICE float sh(const int32 l, const int32 m, const Vector3f& v);
+template <typename Vector3>
+NIH_HOST_DEVICE float sh(const int32 l, const int32 m, const Vector3& v);
+
+template <int32 l, typename Vector3>
+NIH_HOST_DEVICE float sh(const int32 m, const Vector3& v);
+
+template <int32 l, int32 m, typename Vector3>
+NIH_HOST_DEVICE float sh(const Vector3& v);
+
+template <typename ZHVector, typename SHVector, typename Vector3>
+NIH_HOST_DEVICE void rotate_ZH(const int32 L, const ZHVector& zh_coeff, const Vector3& d, SHVector& sh_coeff);
+
+template <int32 L, typename ZHVector, typename SHVector, typename Vector3>
+NIH_HOST_DEVICE void rotate_ZH(const ZHVector& zh_coeff, const Vector3& d, SHVector& sh_coeff);
+
+template <int32 l, int32 m, typename Vector3>
+NIH_HOST_DEVICE float rotate_ZH(const float zh_l, const Vector3& d);
+
 
 template <int32 L>
 struct SH_basis
@@ -42,181 +59,49 @@ struct SH_basis
     static const int32 ORDER  = L;
     static const int32 COEFFS = L*L;
 
-    static NIH_HOST NIH_DEVICE float eval(const int32 i, const Vector3f& d)
+    template <typename Vector3>
+    static NIH_HOST_DEVICE float eval(const int32 i, const Vector3& d)
     {
         if (i == 0)
-            return sh( 0, 0, d );
+            return sh<0>( 0, d );
         else if (i < 4)
-            return sh( 1, i - 2, d );
+            return sh<1>( i - 2, d );
         else if (i < 9)
-            return sh( 2, i - 6, d );
+            return sh<2>( i - 6, d );
         else
-            return sh( 3, i - 12, d );
+            return sh<3>( i - 12, d );
     }
-    static NIH_HOST NIH_DEVICE float eval(const int32 l, const int32 m, const Vector3f& d) { return sh( l, m, d ); }
 
-    static void clamped_cosine(const Vector3f& normal, const float w, float* coeffs)
+    static NIH_HOST_DEVICE void clamped_cosine(const Vector3f& normal, const float w, float* coeffs)
     {
-        const int32 n_samples = 256;
+        const float zh[4] = {
+            0.891209f,
+            1.031964f,
+            0.506579f,
+            0.013224f };
 
-        project(
-            SH_basis<L>(),
-            clamped_cosine_fun<Vector3f>( normal ),
-            w,
-            0.0f,
-            n_samples,
-            coeffs );
+        float sh[COEFFS];
+        rotate_ZH<L>( zh, normal, sh );
+
+        for (uint32 i = 0; i < COEFFS; ++i)
+            coeffs[i] += sh[i] * w;
     }
 
-    static void constant(float k, float* coeffs)
+    static NIH_HOST_DEVICE void constant(float k, float* coeffs)
     {
         coeffs[0] = k * 2.0f*sqrtf(M_PIf);
         for (int32 i = 1; i < COEFFS; ++i)
             coeffs[i] = 0.0f;
     }
 
-    static float integral(const float* coeffs) { return coeffs[0]; }
+    static NIH_HOST_DEVICE float integral(const float* coeffs) { return coeffs[0]; }
 
     template <typename Vector_type>
-    static float integral(const Vector_type& coeffs) { return coeffs[0]; }
+    static NIH_HOST_DEVICE float integral(const Vector_type& coeffs) { return coeffs[0]; }
 
-    static void solve(float* coeffs) {}
+    static NIH_HOST_DEVICE void solve(float* coeffs) {}
 };
 
-template <typename ZHVector, typename SHVector>
-void rotate_ZH(const int32 L, const ZHVector& zh_coeff, const Vector3f& d, SHVector& sh_coeff);
-
-template <typename ZHVector, typename SHVector>
-inline void rotate_ZH(const int32 L, const ZHVector& zh_coeff, const Vector3f& d, SHVector& sh_coeff)
-{
-    for (int32 l = 0; l < L; ++l)
-        for (int32 m = -l; m <= l; ++m)
-            sh_coeff[ l*l + m+l ] = sqrtf( 4.0f*M_PIf / float(2*l + 1) ) * zh_coeff[l] * sh( l, m, d );
-}
-
-NIH_HOST NIH_DEVICE inline float sh(const int32 l, const int32 m, const Vector3f& v)
-{
-#if 0
-    if (l == 0)
-        return 0.282095f;
-    else if (l == 1)
-        return 0.488603f * (m == -1 ? v[0] : (m == 0 ? v[2] : v[1]));
-    else if (l == 2)
-    {
-        if (m == 0)
-            return 0.315392f * (3*v[2]*v[2] - 1.0f);
-        else if (m == 2)
-            return 0.546274f * (v[0]*v[0] - v[1]*v[1]);
-        else if (m == -2)
-            return 1.092548f * v[0]*v[2];
-        else if (m == -1)
-            return 1.092548f * v[1]*v[2];
-        else
-            return 1.092548f * v[0]*v[1];
-    }
-#else
-    const float X = v[0];
-    const float Y = v[1];
-    const float Z = v[2];
-
-    const float m_15_over_4sqrtPI           = 0.54627419f; //sqrtf(15.0f)/(4.0f*sqrtf(M_PIf));
-    const float m_15_over_2sqrtPI           = 1.09254837f; //sqrtf(15.0f)/(2.0f*sqrtf(M_PIf));
-    const float m_5_over_4sqrtPI            = 0.31539154f; //sqrtf(5.0f)/(4.0f*sqrtf(M_PIf));
-    const float m_sqrt2sqrt35_over_8sqrtPI  = 0.59004354f; //sqrtf(2.0f*35.0f)/(8.0f*sqrtf(M_PIf));
-    const float m_sqrt2sqrt35_over_4sqrtPI  = 1.18008709f; //sqrtf(2.0f*35.0f)/(4.0f*sqrtf(M_PIf));
-    const float m_sqrt7_over_4sqrtPI        = 0.37317631f; //sqrtf(7.0f)/(4.0f*sqrtf(M_PIf));
-    const float m_sqrt2sqrt21_over_8sqrtPI  = 0.45704576f; //sqrtf(2.0f*21.0f)/(8.0f*sqrtf(M_PIf));
-    const float m_sqrt105_over_4sqrtPI      = 1.44530571f; //sqrtf(105.0f)/(4.0f*sqrtf(M_PIf));
-    const float m_sqrt105_over_2sqrtPI      = 2.89061141f; //sqrtf(105.0f)/(2.0f*sqrtf(M_PIf));
-
-    if (l == 0)
-        return 0.282095f;
-    else if (l == 1)
-        return 0.488603f * (m == -1 ? -Y : (m == 0 ? Z : -X));
-    else if (l == 2)
-    {
-        if (m == 0)
-            return m_5_over_4sqrtPI * (3*Z*Z - 1.0f);
-        else if (m == 1)
-            return -m_15_over_2sqrtPI * X*Z;
-        else if (m == 2)
-            return m_15_over_4sqrtPI * (X*X - Y*Y);
-        else if (m == -1)
-            return -m_15_over_2sqrtPI * Y*Z;
-        else if (m == -2)
-            return m_15_over_2sqrtPI * X*Y;
-    }
-    else if (l == 3)
-    {
-        if (m == 0)
-            return m_sqrt7_over_4sqrtPI * Z * (5*Z*Z - 3);
-        else if (m == 1)
-            return -m_sqrt2sqrt21_over_8sqrtPI * X * (5*Z*Z - 1);
-        else if (m == 2)
-            return m_sqrt105_over_4sqrtPI * (X*X - Y*Y) * Z;
-        else if (m == 3)
-            return -m_sqrt2sqrt35_over_8sqrtPI * (X*X - 3*Y*Y)*X;
-        else if (m == -1)
-            return -m_sqrt2sqrt21_over_8sqrtPI * Y * (5*Z*Z - 1);
-        else if (m == -2)
-            return m_sqrt105_over_2sqrtPI * X*Y*Z;
-        else if (m == -3)
-            return -m_sqrt2sqrt35_over_8sqrtPI * (3*X*X - Y*Y)*Y;
-    }
-#endif
-    return 0.0f;
-}
-template <int32 l, int32 m>
-NIH_HOST NIH_DEVICE inline float sh(const Vector3f& v)
-{
-    const float X = v[0];
-    const float Y = v[1];
-    const float Z = v[2];
-
-    const float m_15_over_4sqrtPI           = 0.54627419f; //sqrtf(15.0f)/(4.0f*sqrtf(M_PIf));
-    const float m_15_over_2sqrtPI           = 1.09254837f; //sqrtf(15.0f)/(2.0f*sqrtf(M_PIf));
-    const float m_5_over_4sqrtPI            = 0.31539154f; //sqrtf(5.0f)/(4.0f*sqrtf(M_PIf));
-    const float m_sqrt2sqrt35_over_8sqrtPI  = 0.59004354f; //sqrtf(2.0f*35.0f)/(8.0f*sqrtf(M_PIf));
-    const float m_sqrt2sqrt35_over_4sqrtPI  = 1.18008709f; //sqrtf(2.0f*35.0f)/(4.0f*sqrtf(M_PIf));
-    const float m_sqrt7_over_4sqrtPI        = 0.37317631f; //sqrtf(7.0f)/(4.0f*sqrtf(M_PIf));
-    const float m_sqrt2sqrt21_over_8sqrtPI  = 0.45704576f; //sqrtf(2.0f*21.0f)/(8.0f*sqrtf(M_PIf));
-    const float m_sqrt105_over_4sqrtPI      = 1.44530571f; //sqrtf(105.0f)/(4.0f*sqrtf(M_PIf));
-    const float m_sqrt105_over_2sqrtPI      = 2.89061141f; //sqrtf(105.0f)/(2.0f*sqrtf(M_PIf));
-
-    if (l == 0)
-        return 0.282095f;
-    else if (l == 1)
-        return 0.488603f * (m == -1 ? -Y : (m == 0 ? Z : -X));
-    else if (l == 2)
-    {
-        if (m == 0)
-            return m_5_over_4sqrtPI * (3*Z*Z - 1.0f);
-        else if (m == 1)
-            return -m_15_over_2sqrtPI * X*Z;
-        else if (m == 2)
-            return m_15_over_4sqrtPI * (X*X - Y*Y);
-        else if (m == -1)
-            return -m_15_over_2sqrtPI * Y*Z;
-        else if (m == -2)
-            return m_15_over_2sqrtPI * X*Y;
-    }
-    else if (l == 3)
-    {
-        if (m == 0)
-            return m_sqrt7_over_4sqrtPI * Z * (5*Z*Z - 3);
-        else if (m == 1)
-            return -m_sqrt2sqrt21_over_8sqrtPI * X * (5*Z*Z - 1);
-        else if (m == 2)
-            return m_sqrt105_over_4sqrtPI * (X*X - Y*Y) * Z;
-        else if (m == 3)
-            return -m_sqrt2sqrt35_over_8sqrtPI * (X*X - 3*Y*Y)*X;
-        else if (m == -1)
-            return -m_sqrt2sqrt21_over_8sqrtPI * Y * (5*Z*Z - 1);
-        else if (m == -2)
-            return m_sqrt105_over_2sqrtPI * X*Y*Z;
-        else if (m == -3)
-            return -m_sqrt2sqrt35_over_8sqrtPI * (3*X*X - Y*Y)*Y;
-    }
-}
-
 } // namespace nih
+
+#include <nih/spherical/sh_inline.h>
