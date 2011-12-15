5265d6e1e1385d09e741cfeda465554af316b9a1
jpantaleoni <jpantaleoni@gmail.com>
2011-12-15 09:21:08 +0000

    .

---  slash-sandbox/nih/kd/cuda/kd_builder_inline.h
+++  slash-sandbox/nih/kd/cuda/kd_builder_inline.h
@@ -56,81 +56,98 @@ namespace kd {
 
 /// A simple binary tree context implementation to be used with
 /// the Bvh generate() function.
-template <typename Integer>
+template <typename Integer, typename OutputTree>
 struct Kd_context
 {
+    typedef typename OutputTree::Context BaseContext;
+
     /// Cuda accessor struct
     struct Context
     {
         NIH_HOST_DEVICE Context() {}
-        NIH_HOST_DEVICE Context(Kd_node* nodes, uint2* leaves, const Integer* codes, Bbox3f bbox) :
-            m_nodes(nodes), m_leaves(leaves), m_codes( codes ), m_bbox( bbox ) {}
+        NIH_HOST_DEVICE Context(const BaseContext context, const Integer* codes, Bbox3f bbox) :
+            m_context( context ), m_codes( codes ), m_bbox( bbox ) {}
 
         /// write a new node
-        NIH_HOST_DEVICE void write_node(const uint32 node, bool p1, bool p2, const uint32 offset, const uint32 skip_node, const uint32 level, const uint32 split_index)
+        NIH_HOST_DEVICE void write_node(const uint32 node, bool p1, bool p2, const uint32 offset, const uint32 skip_node, const uint32 level, const uint32 begin, const uint32 end, const uint32 split_index)
         {
             if (p1)
             {
                 // fetch the Morton code corresponding to the split plane
-                      uint32 code = m_codes[ split_index ];
-                const uint32 split_dim = level % 3;
+                      Integer code = m_codes[ split_index ];
+                const uint32  split_dim = level % 3;
 
                 // extract the selected coordinate
                 Integer split_coord = 0;
 
+                code >>= level-1;
+                code <<= level-1;
+
                 for (int i = 0; code; i++)
                 {
-	                split_coord |= (((code >> split_dim) & 1) << i);
-                    code >>= 3;
+	                split_coord |= (((code >> split_dim) & 1u) << i);
+                    code >>= 3u;
                 }
 
                 // convert to floating point
                 const float split_plane = convert( m_bbox[0][split_dim], m_bbox[1][split_dim], split_coord );
 
                 // and output the split node
-                m_nodes[ node ] = Kd_node( split_dim, split_plane, offset );
+                m_context.write_node(
+                    node,
+                    offset,
+                    skip_node,
+                    begin,
+                    end,
+                    split_index,
+                    split_dim,
+                    split_plane );
             }
             else
-                m_nodes[ node ] = Kd_node( offset );
+            {
+                // output a leaf node
+                m_context.write_node(
+                    node,
+                    offset,
+                    skip_node,
+                    begin,
+                    end );
+            }
         }
         /// write a new leaf
         NIH_HOST_DEVICE void write_leaf(const uint32 index, const uint32 begin, const uint32 end)
         {
-            m_leaves[ index ] = make_uint2( begin, end );
+            m_context.write_leaf( index, begin, end );
         }
 
-        Kd_node*        m_nodes;    ///< node pointer
-        uint2*          m_leaves;   ///< leaf pointer
+        BaseContext     m_context;
         const Integer*  m_codes;
         Bbox3f          m_bbox;
     };
 
     /// constructor
     Kd_context(
-        thrust::device_vector<Kd_node>* nodes,
-        thrust::device_vector<uint2>*   leaves,
+        OutputTree                   context,
         const Integer*                  codes,
         Bbox3f                          bbox) :
-        m_nodes( nodes ), m_leaves( leaves ), m_codes( codes ), m_bbox( bbox ) {}
+        m_context( context ), m_codes( codes ), m_bbox( bbox ) {}
 
-        /// reserve space for more nodes
-    void reserve_nodes(const uint32 n) { if (m_nodes->size() < n) m_nodes->resize(n); }
+    /// reserve space for more nodes
+    void reserve_nodes(const uint32 n) { m_context.reserve_nodes(n); }
 
     /// reserve space for more leaves
-    void reserve_leaves(const uint32 n) { if (m_leaves->size() < n) m_leaves->resize(n); }
+    void reserve_leaves(const uint32 n) { m_context.reserve_leaves(n); }
 
     /// return a cuda context
     Context get_context()
     {
         return Context(
-            thrust::raw_pointer_cast( &m_nodes->front() ),
-            thrust::raw_pointer_cast( &m_leaves->front() ),
+            m_context.get_context(),
             m_codes,
             m_bbox );
     }
 
-    thrust::device_vector<Kd_node>*  m_nodes;
-    thrust::device_vector<uint2>*    m_leaves;
+    OutputTree                    m_context;
     const Integer*                   m_codes;
     Bbox3f                           m_bbox;
 };
@@ -139,12 +156,14 @@ struct Kd_context
 
 // build a k-d tree given a set of points
 template <typename Integer>
-template <typename Iterator>
+template <typename OutputTree, typename Iterator>
 void Kd_builder<Integer>::build(
-    const Bbox3f    bbox,
-    const Iterator  points_begin,
-    const Iterator  points_end,
-    const uint32    max_leaf_size)
+    OutputTree&                     tree,
+    thrust::device_vector<uint32>&  index,
+    const Bbox3f                    bbox,
+    const Iterator                  points_begin,
+    const Iterator                  points_end,
+    const uint32                    max_leaf_size)
 { 
     typedef cuda::Bintree_gen_context::Split_task Split_task;
 
@@ -152,8 +171,7 @@ void Kd_builder<Integer>::build(
 
     m_bbox = bbox;
     need_space( m_codes, n_points );
-    need_space( *m_index, n_points );
-    need_space( *m_leaves, n_points );
+    need_space( index, n_points );
 
     // compute the Morton code for each point
     thrust::transform(
@@ -166,17 +184,17 @@ void Kd_builder<Integer>::build(
     thrust::copy(
         thrust::counting_iterator<uint32>(0),
         thrust::counting_iterator<uint32>(0) + n_points,
-        m_index->begin() );
+        index.begin() );
 
     // sort the indices by Morton code
     // TODO: use Duane's library directly here... this is doing shameful allocations!
     thrust::sort_by_key(
         m_codes.begin(),
         m_codes.begin() + n_points,
-        m_index->begin() );
+        index.begin() );
 
     // generate a kd-tree
-    kd::Kd_context<Integer> tree( m_nodes, m_leaves, thrust::raw_pointer_cast( &m_codes.front() ), m_bbox );
+    kd::Kd_context<Integer,OutputTree> bintree_context( tree, thrust::raw_pointer_cast( &m_codes.front() ), m_bbox );
 
     const uint32 bits = kd::Morton_bits<Integer>::value;
 
@@ -187,7 +205,7 @@ void Kd_builder<Integer>::build(
         bits,
         max_leaf_size,
         false,
-        tree );
+        bintree_context );
 
     m_leaf_count = m_kd_context.m_leaves;
     m_node_count = m_kd_context.m_nodes;
---  slash-sandbox/nih/kd/cuda/kd_test.cu
+++  slash-sandbox/nih/kd/cuda/kd_test.cu
@@ -26,6 +26,7 @@
  */
 
 #include <nih/kd/cuda/kd_builder.h>
+#include <nih/kd/cuda/kd_context.h>
 #include <nih/sampling/random.h>
 #include <nih/time/timer.h>
 #include <nih/basic/cuda_domains.h>
@@ -33,6 +34,113 @@
 #include <nih/tree/cuda/reduce.h>
 
 namespace nih {
+
+namespace {
+
+bool check_point(
+    const uint32    point_idx,
+    const Vector4f  point,
+    const Kd_node*  nodes,
+    const uint2*    leaves,
+    const uint2*    ranges)
+{
+    uint32 node_index = 0;
+
+    bool success = true;
+
+    while (1)
+    {
+        const Kd_node node  = nodes[ node_index ];
+        const uint2   range = ranges[ node_index ];
+
+        if (point_idx < range.x || point_idx >= range.y)
+        {
+            success = false;
+            break;
+        }
+
+        if (node.is_leaf())
+        {
+            const uint2 leaf = leaves[ node.get_leaf_index() ];
+            success = (point_idx >= leaf.x && point_idx < leaf.y);
+            break;
+        }
+        else
+        {
+            const uint32 split_dim   = node.get_split_dim();
+            const float  split_plane = node.get_split_plane();
+
+            node_index = node.get_child_offset() + 
+                (point[ split_dim ] < split_plane ? 0u : 1u);
+        }
+    }
+
+    if (success)
+        return true;
+
+    // error logging
+    fprintf(stderr, "idx : %u\n", point_idx);
+    fprintf(stderr, "p   : %f, %f, %f\n", point[0], point[1], point[2]);
+
+    node_index = 0;
+
+    while (1)
+    {
+        const Kd_node node  = nodes[ node_index ];
+        const uint2   range = ranges[ node_index ];
+        fprintf(stderr, "\n");
+        fprintf(stderr, "node   : %u\n", node_index);
+        fprintf(stderr, "range  : [%u, %u)\n", range.x, range.y);
+
+        if (point_idx < range.x || point_idx >= range.y)
+        {
+            fprintf(stderr, "out of range!\n");
+            return false;
+        }
+
+        if (node.is_leaf())
+        {
+            const uint2 leaf = leaves[ node.get_leaf_index() ];
+            fprintf(stderr, "leaf : %u = [%u,%u)\n", node.get_leaf_index(), leaf.x, leaf.y);
+            fprintf(stderr, "out of range!\n");
+            return (point_idx >= leaf.x && point_idx < leaf.y);
+        }
+        else
+        {
+            const uint32 split_dim   = node.get_split_dim();
+            const float  split_plane = node.get_split_plane();
+
+            fprintf(stderr, "dim    : %u\n", split_dim);
+            fprintf(stderr, "plane  : %f\n", split_plane);
+            fprintf(stderr, "offset : %u\n", node.get_child_offset());
+
+            node_index = node.get_child_offset() + 
+                (point[ split_dim ] < split_plane ? 0u : 1u);
+        }
+    }
+}
+
+bool check_tree(
+    const uint32    n_points,
+    const Vector4f* points,
+    const Kd_node*  nodes,
+    const uint2*    leaves,
+    const uint2*    ranges)
+{
+    for (uint32 i = 0; i < n_points; ++i)
+    {
+        if (check_point(
+            i,
+            points[i],
+            nodes,
+            leaves,
+            ranges ) == false)
+            return false;
+    }
+    return true;
+}
+
+} // anonymous namespace
 
 void kd_test()
 {
@@ -48,13 +156,14 @@ void kd_test()
         h_points[i] = Vector4f( random.next(), random.next(), random.next(), 1.0f );
 
     thrust::device_vector<Vector4f> d_points( h_points );
-    thrust::device_vector<Vector4f> d_unsorted_points( h_points );
 
     thrust::device_vector<Kd_node>  kd_nodes;
     thrust::device_vector<uint2>    kd_leaves;
+    thrust::device_vector<uint2>    kd_ranges;
     thrust::device_vector<uint32>   kd_index;
 
-    cuda::Kd_builder<uint64> builder( kd_nodes, kd_leaves, kd_index );
+    cuda::Kd_context context( &kd_nodes, &kd_leaves, &kd_ranges );
+    cuda::Kd_builder<uint64> builder;
 
     cudaEvent_t start, stop;
     cudaEventCreate( &start );
@@ -64,13 +173,12 @@ void kd_test()
 
     for (uint32 i = 0; i <= n_tests; ++i)
     {
-        d_points = d_unsorted_points;
-        cudaThreadSynchronize();
-
         float dtime;
         cudaEventRecord( start, 0 );
 
         builder.build(
+            context,
+            kd_index,
             Bbox3f( Vector3f(0.0f), Vector3f(1.0f) ),
             d_points.begin(),
             d_points.end(),
@@ -87,6 +195,30 @@ void kd_test()
 
     cudaEventDestroy( start );
     cudaEventDestroy( stop );
+
+    thrust::host_vector<uint32>   h_kd_index( kd_index );
+    thrust::host_vector<Kd_node>  h_kd_nodes( kd_nodes );
+    thrust::host_vector<uint2>    h_kd_leaves( kd_leaves );
+    thrust::host_vector<uint2>    h_kd_ranges( kd_ranges );
+
+    thrust::host_vector<Vector4f> h_sorted_points( n_points );
+
+    thrust::gather(
+        h_kd_index.begin(),
+        h_kd_index.begin() + n_points,
+        h_points.begin(),
+        h_sorted_points.begin() );
+
+    if (check_tree(
+        n_points,
+        thrust::raw_pointer_cast( &h_sorted_points.front() ),
+        thrust::raw_pointer_cast( &h_kd_nodes.front() ),
+        thrust::raw_pointer_cast( &h_kd_leaves.front() ),
+        thrust::raw_pointer_cast( &h_kd_ranges.front() ) ) == false)
+    {
+        fprintf(stderr, "k-d tree test... *** failed ***\n");
+        exit(1);
+    }
 
     fprintf(stderr, "k-d tree test... done\n");
     fprintf(stderr, "  time       : %f ms\n", time * 1000.0f );
