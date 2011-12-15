c4329f0353e969b986c2f002ae6feec06782dd9d
jpantaleoni@gmail.com <jpantaleoni@gmail.com@0b0c87b0-e858-3a96-bd32-8cfd005e83f8>
2011-12-15 15:31:49 +0000

    .

--- trunk/ slash-sandbox/nih/kd/cuda/knn_inline.h
+++ trunk/ slash-sandbox/nih/kd/cuda/knn_inline.h
@@ -179,8 +179,169 @@ __device__ void lookup(
     results->dist2 = dist2;
 }
 
+struct Compare
+{
+    FORCE_INLINE NIH_HOST_DEVICE bool operator() (const float2 op1, const float2 op2) const
+    {
+        return (op1.y == op2.y) ?
+            op1.x < op2.x :
+            op1.y < op2.y;
+    }
+};
+
+template <uint32 K, typename VectorType, typename PointIterator>
+__device__ void lookup(
+    const VectorType        query,
+    const Kd_node*          kd_nodes,
+    const uint2*            kd_ranges,
+    const uint2*            kd_leaves,
+    const PointIterator     kd_points,
+    Kd_knn<3>::Result*      results)
+{
+    //
+    // 1-st pass: find the smallest node containing the query point and compute an upper bound
+    // on the search distance
+    //
+
+    priority_queue<float2,Compare,K> queue;
+    float max_dist2 = 1.0e16f;
+
+    // start from the root node
+    uint32 node_index = 0;
+
+    // keep track of which node we visited
+    uint32 first_node = 0;
+
+    while (1)
+    {
+        const Kd_node node = kd_nodes[ node_index ];
+        const uint2 range = kd_ranges[ node_index ];
+
+        if (range.y - range.x < K)
+            break;
+
+        first_node = node_index;
+
+        if (node.is_leaf())
+            break;
+        else
+        {
+            const uint32 split_dim   = node.get_split_dim();
+            const float  split_plane = node.get_split_plane();
+
+            node_index =
+                split_dim == 0 ? node.get_child_offset() + (query[0] < split_plane ? 0u : 1u) :
+                split_dim == 1 ? node.get_child_offset() + (query[1] < split_plane ? 0u : 1u) :
+                                 node.get_child_offset() + (query[2] < split_plane ? 0u : 1u);
+        }
+    }
+    // process all points in the found node
+    {
+        // find the closest neighbor in this node
+        const uint2 range = kd_ranges[ first_node ];
+        for (uint32 i = range.x; i < range.y; ++i)
+        {
+            const VectorType delta = kd_points[i] - query;
+            const float d2 = delta[0]*delta[0] + delta[1]*delta[1] + delta[2]*delta[2];
+            queue.push( make_float2( binary_cast<float>(i), d2 ) );
+        }
+        // set the maximum distance bound
+        max_dist2 = queue.top().y;
+    }
+
+    //
+    // 2-nd pass: visit the tree with a stack and careful pruning
+    //
+
+    int32  stackp = 1;
+    float4 stack[32];
+
+    // place a sentinel node in the stack
+    stack[0] = make_float4( 0.0f, 0.0f, 0.0f, binary_cast<float>(uint32(-1)) );
+
+    // start from the root node
+    node_index = 0;
+
+    float3 cdist = make_float3( 0.0f, 0.0f, 0.0f );
+
+    while (node_index != uint32(-1))
+    {
+        const Kd_node node = kd_nodes[ node_index ];
+
+        if (node.is_leaf() || first_node == node_index)
+        {
+            if (first_node != node_index)
+            {
+                // find the closest neighbor in this leaf
+                const uint2 leaf = kd_leaves[ node.get_leaf_index() ];
+                for (uint32 i = leaf.x; i < leaf.y; ++i)
+                {
+                    const VectorType delta = kd_points[i] - query;
+                    const float d2 = delta[0]*delta[0] + delta[1]*delta[1] + delta[2]*delta[2];
+                    if (max_dist2 > d2)
+                    {
+                        queue.push( make_float2( binary_cast<float>(i), d2 ) );
+
+                        // reset the maximum distance bound
+                        max_dist2 = queue.top().y;
+                    }
+                }
+            }
+
+            // pop the next node from the stack
+            while (stackp > 0)
+            {
+                const float4 stack_node = stack[ --stackp ];
+                node_index = binary_cast<uint32>( stack_node.w );
+                cdist      = make_float3( stack_node.x, stack_node.y, stack_node.z );
+
+                if (cdist.x*cdist.x + cdist.y*cdist.y + cdist.z*cdist.z > max_dist2)
+                    continue;
+            }
+        }
+        else
+        {
+            const uint32 split_dim   = node.get_split_dim();
+            const float  split_plane = node.get_split_plane();
+
+            const float split_dist = 
+                split_dim == 0 ? (query[0] - split_plane) :
+                split_dim == 1 ? (query[1] - split_plane) :
+                                 (query[2] - split_plane);
+
+            const uint32 select = split_dist <= 0.0f ? 0u : 1u;
+
+            node_index = node.get_child_offset() + select;
+
+            // compute the vector distance to the far node
+            float3 cdist_far = cdist;
+            if (split_dim == 0)      cdist_far.x = split_dist;
+            else if (split_dim == 1) cdist_far.y = split_dist;
+            else                     cdist_far.z = split_dist;
+
+            // check whether we should push the far node on the stack
+            const float dist_far2 = cdist_far.x*cdist_far.x +
+                                    cdist_far.y*cdist_far.y +
+                                    cdist_far.z*cdist_far.z;
+
+            if (dist_far2 <= max_dist2)
+            {
+                stack[ stackp++ ] = make_float4(
+                    cdist_far.x,
+                    cdist_far.y,
+                    cdist_far.z,
+                    binary_cast<float>( node.get_child_offset() + 1u - select ) );
+            }
+        }
+    }
+
+    // write the results
+    for (uint32 i = 0; i < K; ++i)
+        ((float2*)results)[i] = queue[i];
+}
+
 template <typename QueryIterator, typename PointIterator>
-__global__ void lookup_kernel(
+__global__ void lookup_kernel_1(
     const uint32                    n_points,
     const QueryIterator             points_begin,
     const Kd_node*                  kd_nodes,
@@ -210,6 +371,37 @@ __global__ void lookup_kernel(
     }
 }
 
+template <uint32 K, typename QueryIterator, typename PointIterator>
+__global__ void lookup_kernel(
+    const uint32                    n_points,
+    const QueryIterator             points_begin,
+    const Kd_node*                  kd_nodes,
+    const uint2*                    kd_ranges,
+    const uint2*                    kd_leaves,
+    const PointIterator             kd_points,
+    Kd_knn<3>::Result*              results)
+{
+    const uint32 grid_size = gridDim.x * blockDim.x;
+
+    // loop through all logical blocks associated to this physical one
+    for (uint32 base_idx = blockIdx.x * blockDim.x;
+                base_idx < n_points;
+                base_idx += grid_size)
+    {
+        const uint32 index = threadIdx.x + base_idx;
+        if (index >= n_points)
+            return;
+
+        lookup<K>(
+            thrust::detail::backend::dereference( points_begin + index ),
+            kd_nodes,
+            kd_ranges,
+            kd_leaves,
+            kd_points,
+            results + index*K );
+    }
+}
+
 } // namespace knn
 
 // perform a k-nn lookup for a set of query points
@@ -234,10 +426,47 @@ void Kd_knn<3>::run(
 
     const uint32 BLOCK_SIZE = 128;
     const size_t max_blocks = thrust::detail::backend::cuda::arch::max_active_blocks(
-        kd_knn::lookup_kernel<QueryIterator,PointIterator>, BLOCK_SIZE, 0);
+        kd_knn::lookup_kernel_1<QueryIterator,PointIterator>, BLOCK_SIZE, 0);
     const size_t n_blocks   = nih::min( max_blocks, (n_points + BLOCK_SIZE-1) / BLOCK_SIZE );
 
-    kd_knn::lookup_kernel<<<n_blocks,BLOCK_SIZE>>> (
+    kd_knn::lookup_kernel_1<<<n_blocks,BLOCK_SIZE>>> (
+        n_points,
+        points_begin,
+        kd_nodes,
+        kd_ranges,
+        kd_leaves,
+        kd_points,
+        results );
+
+    cudaThreadSynchronize();
+}
+
+// perform a k-nn lookup for a set of query points
+//
+// \param points_begin     beginning of the query point sequence
+// \param points_end       end of the query point sequence
+// \param kd_nodes         k-d tree nodes
+// \param kd_ranges        k-d tree node ranges
+// \param kd_leaves        k-d tree leaves
+// \param kd_points        k-d tree points
+template <uint32 K, typename QueryIterator, typename PointIterator>
+void Kd_knn<3>::run(
+    const QueryIterator             points_begin,
+    const QueryIterator             points_end,
+    const Kd_node*                  kd_nodes,
+    const uint2*                    kd_ranges,
+    const uint2*                    kd_leaves,
+    const PointIterator             kd_points,
+    Result*                         results)
+{
+    const uint32 n_points = uint32( points_end - points_begin );
+
+    const uint32 BLOCK_SIZE = 64;
+    const size_t max_blocks = thrust::detail::backend::cuda::arch::max_active_blocks(
+        kd_knn::lookup_kernel<K,QueryIterator,PointIterator>, BLOCK_SIZE, 0);
+    const size_t n_blocks   = nih::min( max_blocks, (n_points + BLOCK_SIZE-1) / BLOCK_SIZE );
+
+    kd_knn::lookup_kernel<K> <<<n_blocks,BLOCK_SIZE>>> (
         n_points,
         points_begin,
         kd_nodes,
